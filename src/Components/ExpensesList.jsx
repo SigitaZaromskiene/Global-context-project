4f2350a300d4cd4bafe298c91ce55de6863ccf3c
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-06-21 14:30:20 +0300

    message

--- src/Components/ExpensesList.jsx
+++ src/Components/ExpensesList.jsx
@@ -8,17 +8,21 @@ function ExpensesList({ dataList, year }) {
   const filtered = dataList.filter((d) => d.date.slice(0, 4) === year);
   return (
     <>
-      {filtered.map((d) => (
-        <div key={uuidv4()} className="form-container">
-          <div className="form-content">
-            <p>{d.expense}</p>
-            <div style={{ display: "flex", gap: "40px", width: "280px" }}>
-              <p>{d.sum} &euro;</p>
-              <p>{d.date}</p>
+      {filtered.length === 0 ? (
+        <p style={{ fontSize: "24px", color: "white" }}>No expenses found</p>
+      ) : (
+        filtered.map((d) => (
+          <div key={uuidv4()} className="form-container">
+            <div className="form-content">
+              <p>{d.expense}</p>
+              <div style={{ display: "flex", gap: "40px", width: "280px" }}>
+                <p>{d.sum} &euro;</p>
+                <p>{d.date}</p>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))
+      )}
     </>
   );
 }
